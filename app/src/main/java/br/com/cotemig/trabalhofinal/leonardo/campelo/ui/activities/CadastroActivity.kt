082101d1f2c1f06c71742c6072a299d02d67bac6
Leonardo Campelo <lp.campelo@outlook.com>
2021-12-01 18:03:46 -0300

    Cadastro de conta funcional

---
+++ app/src/main/java/br/com/cotemig/trabalhofinal/leonardo/campelo/ui/activities/CadastroActivity.kt
@@ -0,0 +1,11 @@
+package br.com.cotemig.trabalhofinal.leonardo.campelo.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CadastroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro)
+    }
+}
